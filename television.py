2901f135f4c25396944369c4a3cf2c6140925e6a
kevbowevbo <kevin.gomez0528@gmail.com>
2024-11-22 16:52:00 -0600

    adding doc strings and type hinting for branch called testing

--- television.py
+++ television.py
@@ -1,23 +1,36 @@
+#Adding doc strings and type hinting to television for new branch called test
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    """
+    A class to set up a television and it's details like volume, power status, and channel
+    """
+    MIN_VOLUME :int = 0  #Default Minimum volume set at 0
+    MAX_VOLUME :int = 2  #Default Maximim Volume set at 2
+    MIN_CHANNEL :int = 0 #Default Minimum channel set at 0
+    MAX_CHANNEL :int = 3 #Default Maximum channel set at 3
 
 
     def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = Television.MIN_VOLUME
-        self.__channel = Television.MIN_CHANNEL
+        """
+        Function to set up default instance Variables
+        """
+        self.__status :bool = False
+        self.__muted :bool = False
+        self.__volume :int = Television.MIN_VOLUME
+        self.__channel :int = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Function to turn tv on and off by switching the status variable
+        """
         if self.__status == False:
-            self.__status = True
+            self.__status :bool = True
         elif self.__status == True:
-            self.__status = False
+            self.__status :bool = False
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Function sees if the tv is on and if it is, it'll mute/unmutes the tv
+        """
         if self.__status == True:
             if self.__muted == False:
                 self.__muted = True
@@ -25,28 +38,40 @@ class Television:
                 self.__muted = False
 
 
-    def channel_up(self):
+    def channel_up(self) ->None:
+        """
+        Function sees if tv is on and if it is it'll change the channel up one
+        """
         if self.__status == True:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Function sees if tv is on and if it is it'll change the channel down one
+        """
         if self.__status == True:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Function sees if tv is on and if it is it'll change the volume up one
+        """
         if self.__status == True:
-            self.__muted= False
+            self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
             else:
                 self.__volume = Television.MAX_VOLUME
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Function sees if tv is on and if it is it'll change the volume down one
+        """
         if self.__status == True:
             self.__muted = False
             if self.__volume == Television.MIN_VOLUME:
@@ -54,7 +79,11 @@ class Television:
             else:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Function gives the format of strings when they are called to be printed
+        :return: TV status and values of volume and channels
+        """
         if self.__muted == False:
             return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
         else:
