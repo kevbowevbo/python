1780ec46a51506ec527f92a8121569f2fbce37af
kevbowevbo <kevin.gomez0528@gmail.com>
2024-11-22 17:14:12 -0600

    adding test of television

---
+++ test_television.py
@@ -0,0 +1,37 @@
+import pytest
+from television import *
+
+
+
+class Test:
+    def setup_method(self):
+
+
+
+
+    def test_init(self):
+        assert
+
+    def test_power():
+        assert pytest.raises()
+
+
+    def test_mute():
+
+
+
+    def test_channel_up():
+
+
+
+    def test_channel_down():
+
+
+
+
+    def test_volume_up():
+
+
+
+
+    def test_volume_down():
